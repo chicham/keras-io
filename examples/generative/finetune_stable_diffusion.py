9fe2227841eb22852df239c2eb4a326c95212940
Hicham Randrianarivo <hicham+github@randrianarivo.net>
2024-01-14 17:50:58 +0100

    change: update imports

--- examples/generative/finetune_stable_diffusion.py
+++ examples/generative/finetune_stable_diffusion.py
@@ -83,12 +83,8 @@ import numpy as np
 import pandas as pd
 import tensorflow as tf
 import tensorflow.experimental.numpy as tnp
-from keras_cv.models.stable_diffusion.clip_tokenizer import SimpleTokenizer
-from keras_cv.models.stable_diffusion.diffusion_model import DiffusionModel
-from keras_cv.models.stable_diffusion.image_encoder import ImageEncoder
-from keras_cv.models.stable_diffusion.noise_scheduler import NoiseScheduler
-from keras_cv.models.stable_diffusion.text_encoder import TextEncoder
-from tensorflow import keras
+from keras_cv.models.stable_diffusion import SimpleTokenizer, DiffusionModel, ImageEncoder, NoiseScheduler, TextEncoder
+import keras
 
 """
 ## Data loading
